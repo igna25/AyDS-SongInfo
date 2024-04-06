990f8335041d2087fcf3418425299ccb3781a215
JuanOcaranza <68519407+JuanOcaranza@users.noreply.github.com>
2024-04-06 16:18:09 -0300

    Aplicamos SOLID a SongDescriptionHelperImpl

--- app/src/main/java/ayds/songinfo/home/view/SongDescriptionHelperImpl.kt
+++ app/src/main/java/ayds/songinfo/home/view/SongDescriptionHelperImpl.kt
@@ -1,17 +1,14 @@
 package ayds.songinfo.home.view
 
-import android.icu.text.SimpleDateFormat
 import ayds.songinfo.home.model.entities.Song.EmptySong
 import ayds.songinfo.home.model.entities.Song
 import ayds.songinfo.home.model.entities.Song.SpotifySong
-import java.time.Year
-import java.util.Date
 
 interface SongDescriptionHelper {
     fun getSongDescriptionText(song: Song = EmptySong): String
 }
 
-internal class SongDescriptionHelperImpl : SongDescriptionHelper {
+internal class SongDescriptionHelperImpl(private val releaseDateHelper: ReleaseDateHelper) : SongDescriptionHelper {
     override fun getSongDescriptionText(song: Song): String {
         return when (song) {
             is SpotifySong ->
@@ -21,21 +18,8 @@ internal class SongDescriptionHelperImpl : SongDescriptionHelper {
                 }\n" +
                         "Artist: ${song.artistName}\n" +
                         "Album: ${song.albumName}\n" +
-                        "Release date: ${releaseDateString(song)}"
+                        "Release date: ${releaseDateHelper.getReleaseDateString(song)}"
             else -> "Song not found"
         }
-    }
-
-    private fun releaseDateString(song: SpotifySong): String {
-        return when (song.releaseDatePrecision) {
-            "day" -> SimpleDateFormat("dd/MM/yyyy").format(SimpleDateFormat("yyyy-MM-dd").parse(song.releaseDate))
-            "month" -> SimpleDateFormat("MMMM, yyyy").format(SimpleDateFormat("yyyy-MM").parse(song.releaseDate))
-            "year" -> toYearLeapOrNotFormat(song.releaseDate.split("-").first().toLong())
-            else -> "Invalid released date precision"
-        }
-    }
-
-    private fun toYearLeapOrNotFormat(year: Long): String {
-        return "$year (${if (Year.isLeap(year)) "" else "Not a "}leap year)"
     }
 }
